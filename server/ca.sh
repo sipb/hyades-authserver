a5cfed2ea581963cd77dca5c75c5ad5e675855d9
Sam Dukhovni <dukhovni@mit.edu>
2017-05-04 22:42:21 -0400

    Quote variables for safety, even if we trust mktemp

--- server/ca.sh
+++ server/ca.sh
@@ -3,7 +3,7 @@ set -e
 
 ./check_knc_creds.py authorized || exit 1
 TMPDIR=$(mktemp -d)
-cat > $TMPDIR/client_key.pub
-ssh-keygen -s ca_key -I "$KNC_CREDS" -n root,dev -V +4h $TMPDIR/client_key.pub
-cat $TMPDIR/client_key-cert.pub
-rm -rf $TMPDIR
+cat > "$TMPDIR"/client_key.pub
+ssh-keygen -s ca_key -I "$KNC_CREDS" -n root,dev -V +4h "$TMPDIR"/client_key.pub
+cat "$TMPDIR"/client_key-cert.pub
+rm -rf "$TMPDIR"
